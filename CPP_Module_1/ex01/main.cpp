df4ae36c0edde2291540fb2988933bd607186cbd
Yekta Soytürk <github@inertia.slmail.me>
2026-04-23 00:50:20 +0300

    zombieHorde copy fixed

--- CPP_Module_1/ex01/main.cpp
+++ CPP_Module_1/ex01/main.cpp
@@ -6,13 +6,13 @@
 /*   By: yesoytur <yesoytur@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:44:48 by yesoytur          #+#    #+#             */
-/*   Updated: 2026/04/22 12:44:49 by yesoytur         ###   ########.fr       */
+/*   Updated: 2026/04/23 00:48:59 by yesoytur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* zombieHorde(int N, std::string name);
+Zombie* zombieHorde(int N, const std::string& name);
 
 int main(void) {
     int N = 5;
--- CPP_Module_1/ex01/zombieHorde.cpp
+++ CPP_Module_1/ex01/zombieHorde.cpp
@@ -6,13 +6,13 @@
 /*   By: yesoytur <yesoytur@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:44:08 by yesoytur          #+#    #+#             */
-/*   Updated: 2026/04/22 12:44:10 by yesoytur         ###   ########.fr       */
+/*   Updated: 2026/04/23 00:48:44 by yesoytur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* zombieHorde(int N, std::string name) {
+Zombie* zombieHorde(int N, const std::string& name) {
     Zombie* horde = new Zombie[N];
     for (int i = 0; i < N; i++)
         horde[i].setName(name);
