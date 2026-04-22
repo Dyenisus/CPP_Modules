534b0bdb93c7fc9ab6f2697821832ac4f9db007e
Yekta Soytürk <github@inertia.slmail.me>
2026-04-23 00:48:11 +0300

    newZombie copy fixed

--- CPP_Module_1/ex00/main.cpp
+++ CPP_Module_1/ex00/main.cpp
@@ -6,13 +6,13 @@
 /*   By: yesoytur <yesoytur@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:37:18 by yesoytur          #+#    #+#             */
-/*   Updated: 2026/04/22 12:37:19 by yesoytur         ###   ########.fr       */
+/*   Updated: 2026/04/23 00:46:27 by yesoytur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name);
+Zombie* newZombie(const std::string& name);
 void randomChump(std::string name);
 
 int main(void) {
--- CPP_Module_1/ex00/newZombie.cpp
+++ CPP_Module_1/ex00/newZombie.cpp
@@ -6,12 +6,12 @@
 /*   By: yesoytur <yesoytur@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:37:23 by yesoytur          #+#    #+#             */
-/*   Updated: 2026/04/22 12:37:24 by yesoytur         ###   ########.fr       */
+/*   Updated: 2026/04/23 00:46:34 by yesoytur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* newZombie(std::string name) {
+Zombie* newZombie(const std::string& name) {
     return new Zombie(name);
 }
